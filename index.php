ec4dc97a0e6cbdfb32785bf4fe2ed03b03ff8ad9
Max Mazzocchi <mmazzocc@mines.edu>
2012-10-24 11:13:23 -0600

    Website skeleton added.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+
+<html>
+    <head>
+        <title>Team 12 Game</title>
+        <link rel=stylesheet href="style.css" type="text/css" />
+
+    </head>
+    <body>
+        <div class=content>
+        </div>
+    </body>
+</html>
