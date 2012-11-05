2c5d8834cc1c82ebc6876625391a16dff8745b03
Max Mazzocchi <mmazzocc@mines.edu>
2012-11-04 21:19:03 -0700

    Payouts displayed correctly

--- frontend.js
+++ frontend.js
@@ -60,16 +60,16 @@ function drawScore(ctx) {
 
 function drawPayoutTable(ctx) {
     var winningHands = [
-        "Royal Flush",
-        "Straight Flush",
-        "Four Aces",
-        "Four Of A Kind",
-        "Full House",
-        "Flush",
-        "Straight",
-        "Three Of A Kind",
-        "Two Pair",
-        "Jacks Or Better"
+        ["Royal Flush", 250],
+        ["Straight Flush", 150],
+        ["Four Aces", 100],
+        ["Four Of A Kind", 50],
+        ["Full House", 10],
+        ["Flush", 5],
+        ["Straight", 4],
+        ["Three Of A Kind", 3],
+        ["Two Pair", 2],
+        ["Jacks Or Better", 1]
     ];
 
     var x=60;
@@ -85,9 +85,9 @@ function drawPayoutTable(ctx) {
         ctx.strokeRect(x+200,y,200,15);
         ctx.strokeRect(x+400,y,200,15);
         ctx.lineWidth = 1;
-        ctx.strokeText(v,x+2,y+12);
-        ctx.strokeText((10-i),x+202,y+12);
-        ctx.strokeText((bet*(10-i)),x+402,y+12);
+        ctx.strokeText(v[0],x+2,y+12);
+        ctx.strokeText(v[1],x+202,y+12);
+        ctx.strokeText((bet*v[1]),x+402,y+12);
         y+=15;
     });
 }
